2f764924cc7b4a278c968d0b424dfd9b834a955f
kangoadrian <87994692+kangoadrian@users.noreply.github.com>
2023-07-30 15:08:45 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Result from './components/Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='react-calculator'>
+        <Result></Result>
+        <div>
+          Seccion para los numeros
+        </div>
+        <div>
+           Funciones de la calculadora
+        </div>
+        <div>
+          Operadores
+        </div>
     </div>
   );
 }
